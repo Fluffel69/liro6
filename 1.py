c4ecb54eec47206c8e81f164eae8ec7ada382ea6
Fluffel69 <114808361+Fluffel69@users.noreply.github.com>
2023-05-13 14:13:29 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -10,13 +10,13 @@ combo=Combobox(window)
 result=Label(window,text='',font=('Kegel',50))
 def f():
     if '+' in ent1.get():
-        result.configure(text=int(ent1.get()[0])+int(ent1.get()[2]))
+        result.configure(text=int(ent1.get().partition('+')[0])+int(ent1.get().partition('+')[2]))
     if '-' in ent1.get():
-        result.configure(text=int(ent1.get()[0])-int(ent1.get()[2]))
+        result.configure(text=int(ent1.get().partition('-')[0])-int(ent1.get().partition('-')[2]))
     if '*' in ent1.get():
-        result.configure(text=int(ent1.get()[0])*int(ent1.get()[2]))
+        result.configure(text=int(ent1.get().partition('*')[0])*int(ent1.get().partition('*')[2]))
     if '/' in ent1.get():
-        result.configure(text=int(ent1.get()[0])/int(ent1.get()[2]))
+        result.configure(text=int(ent1.get().partition('/')[0])/int(ent1.get().partition('/')[2]))
 def h1():
     ent1.insert(END,1)
 def h2():
@@ -47,6 +47,8 @@ def h14():
     ent1.delete(ent1.index('end')-1)
 def h15():
     ent1.delete(0,END)
+def h16():
+    result.configure(text='')
 combo['values']=('plus','minus','mult','division')
 
 
@@ -54,22 +56,23 @@ ent1=Entry(window,width='10')
 ent1.focus()
 
 
-btn=Button(window,bg='white',text='Ecuals',width='5',height='2',command=f)
-btn1=Button(window,text='1',bg='Green',width='5',height='2',command=h1)
-btn2=Button(window,text='2',bg='Green',width='5',height='2',command=h2)
-btn3=Button(window,text='3',bg='Green',width='5',height='2',command=h3)
-btn4=Button(window,text='4',bg='Green',width='5',height='2',command=h4)
-btn5=Button(window,text='5',bg='Green',width='5',height='2',command=h5)
-btn6=Button(window,text='6',bg='Green',width='5',height='2',command=h6)
-btn7=Button(window,text='7',bg='Green',width='5',height='2',command=h7)
-btn8=Button(window,text='8',bg='Green',width='5',height='2',command=h8)
-btn9=Button(window,text='9',bg='Green',width='5',height='2',command=h9)
-btnq=Button(window,text='+',bg='Green',width='5',height='2',command=h10)
-btnw=Button(window,text='-',bg='Green',width='5',height='2',command=h11)
-btne=Button(window,text='*',bg='Green',width='5',height='2',command=h12)
-btnr=Button(window,text='/',bg='Green',width='5',height='2',command=h13)
-btn_del=Button(window,text='delete',bg='Aqua',width='5',height='2',command=h14)
-btn_cl=Button(window,text='clear',bg='Red',width='5',height='2',command=h15)
+btn=Button(window,bg='white',text='Ecuals',width='10',height='2',command=f)
+btn1=Button(window,text='1',bg='Green',width='10',height='2',command=h1)
+btn2=Button(window,text='2',bg='Green',width='10',height='2',command=h2)
+btn3=Button(window,text='3',bg='Green',width='10',height='2',command=h3)
+btn4=Button(window,text='4',bg='Green',width='10',height='2',command=h4)
+btn5=Button(window,text='5',bg='Green',width='10',height='2',command=h5)
+btn6=Button(window,text='6',bg='Green',width='10',height='2',command=h6)
+btn7=Button(window,text='7',bg='Green',width='10',height='2',command=h7)
+btn8=Button(window,text='8',bg='Green',width='10',height='2',command=h8)
+btn9=Button(window,text='9',bg='Green',width='10',height='2',command=h9)
+btnq=Button(window,text='+',bg='Green',width='10',height='2',command=h10)
+btnw=Button(window,text='-',bg='Green',width='10',height='2',command=h11)
+btne=Button(window,text='*',bg='Green',width='10',height='2',command=h12)
+btnr=Button(window,text='/',bg='Green',width='10',height='2',command=h13)
+btn_del=Button(window,text='delete',bg='Aqua',width='10',height='2',command=h14)
+btn_cl_en=Button(window,text='clear entry',bg='Red',width='10',height='2',command=h15)
+btn_cl_re=Button(window,text='clear result',bg='Pink',width='10',height='2',command=h16)
 
 btn1.grid(column=0,row=5)
 btn2.grid(column=1,row=5)
@@ -86,7 +89,8 @@ btne.grid(column=2,row=7)
 btnr.grid(column=3,row=7)
 btn.grid(column=4,row=6)
 btn_del.grid(column=0,row=8)
-btn_cl.grid(column=1,row=8)
-result.grid(column=5,row=6)
+btn_cl_en.grid(column=0,row=9)
+btn_cl_re.grid(column=0,row=10)
+result.grid(column=2,row=8)
 ent1.grid(column=5,row=7)
 window.mainloop()
